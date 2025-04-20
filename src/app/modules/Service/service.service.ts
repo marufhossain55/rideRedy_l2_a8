2d56b50390a4071fe18ae338239b6464b87baf33
Your Name <you@example.com>
2025-04-20 09:11:31 +0600

    service added

---
+++ src/app/modules/Service/service.service.ts
@@ -0,0 +1,103 @@
+import { ServiceRecord, ServiceStatus } from '@prisma/client';
+import prisma from '../../../utils/prisma';
+
+const createService = async (serviceData: ServiceRecord) => {
+  const isBikeExist = await prisma.bike.findUnique({
+    where: {
+      bikeId: serviceData.bikeId,
+    },
+  });
+
+  if (!isBikeExist) {
+    throw new Error('Bike Does not exist.');
+  }
+
+  const result = await prisma.serviceRecord.create({
+    data: serviceData,
+  });
+
+  return result;
+};
+
+const getAllService = async () => {
+  const result = await prisma.serviceRecord.findMany();
+  return result;
+};
+
+const getServiceById = async (serviceId: string) => {
+  const isExist = await prisma.serviceRecord.findUnique({
+    where: {
+      serviceId,
+    },
+  });
+
+  if (!isExist) {
+    throw new Error('Service Record Not Found.');
+  }
+  const result = await prisma.serviceRecord.findUnique({
+    where: {
+      serviceId,
+    },
+  });
+  return result;
+};
+
+const updateServiceById = async (
+  serviceId: string,
+  serviceData: Partial<ServiceRecord>
+): Promise<ServiceRecord> => {
+  const isExist = await prisma.serviceRecord.findUnique({
+    where: {
+      serviceId,
+    },
+  });
+
+  if (!isExist) {
+    throw new Error('Service Not Found.');
+  }
+
+  const result = await prisma.serviceRecord.update({
+    where: {
+      serviceId,
+    },
+    data: {
+      ...serviceData,
+      status: ServiceStatus.done,
+    },
+  });
+
+  return result;
+};
+
+export const fetchOverdueOrPendingServices = async () => {
+  const sevenDaysAgo = new Date();
+
+  sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+  const result = await prisma.serviceRecord.findMany({
+    where: {
+      AND: [
+        {
+          status: {
+            in: [ServiceStatus.pending, ServiceStatus.in_progress],
+          },
+        },
+        {
+          serviceDate: {
+            lt: sevenDaysAgo,
+          },
+        },
+      ],
+    },
+  });
+
+  return result;
+};
+
+export const ServiceDataService = {
+  createService,
+  getAllService,
+  getServiceById,
+  updateServiceById,
+  fetchOverdueOrPendingServices,
+};
